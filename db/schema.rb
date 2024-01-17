15b4d7aa23dfeea74317626633a0e32e5729bd2a
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 16:59:15 -0700

    Change column :percent to :discount_type for coder empathy reasons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_210846) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_234324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_17_210846) do
     t.string "name"
     t.string "code"
     t.integer "amount"
-    t.integer "percent"
+    t.integer "type"
     t.integer "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
