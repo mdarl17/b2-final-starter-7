1eacf8742a5f1e3a79576072f741f40926fc39a9
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 12:18:52 -0700

    Add condition and sad path resolution for situation when a user tries to create a new coupon that would exceed the provided req limit of five active coupons per merchant at a time

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,15 +12,17 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
-  def create 
+  def create
     merchant = Merchant.find(params[:merchant_id])
     coupon = Coupon.new(coupon_params)
-    
-    if coupon.save
+    if merchant.five_active_coupons?
+      flash[:alert] = "Merchants may have only five active coupons. Please de-activate a coupon from this merchant before trying to create another one."
+    elsif coupon.save
       merchant.coupons << coupon 
     else
-      flash[:alert] = "There was a problem and the merchant coupon could not be saved."
+      flash[:alert] = "There was a problem creating this coupon. Please try again later."
     end
+    redirect_to merchant_coupons_path(merchant.id)
   end
 
   private
