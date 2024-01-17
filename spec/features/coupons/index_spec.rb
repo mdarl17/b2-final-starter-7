75758a476dd569d333adb13d0dac075f701fad8b
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 12:27:37 -0700

    Add more seed data for @m1, along with feature tests to ensure new `exceeds five active coupons` functionality works as expected.

--- spec/features/coupons/index_spec.rb
+++ spec/features/coupons/index_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe "merchant coupons index", type: :feature do
     @m1 = Merchant.create!(name: "Merchant 1")
     @m2 = Merchant.create!(name: "Merchant 2")
     @m3 = Merchant.create!(name: "Merchant 3", status: 1)
-    @m1_bogo = Coupon.create!(name: "Buy one item, get that same item free!", code: "M1_BOGO", amount: 50, percent: 1, merchant_id: @m1.id)
-    @m1_half = Coupon.create!(name: "50% off any item", code: "M1_HALF_OFF1", amount: 50, percent: 1, merchant_id: @m1.id)
-    @m1_buy3 = Coupon.create!(name: "Buy three items of any kind, get the fourth free!", code: "M1_BUY3_4FREE", amount: 25, percent: 1, merchant_id: @m1.id)
-    @m2_10 = Coupon.create!(name: "$10 off any item!", code: "M2_10OFF", amount: 10, percent: 0, merchant_id: @m2.id)
-    @m3_2024 = Coupon.create!(name: "$24 off an order of $100 or more", code: "M3_24", amount: 24, percent: 0, merchant_id: @m3.id)
-    @m3_blowout = Coupon.create!(name: "50% off entire order!", code: "M3_TOTAL50", amount: 50, percent: 1, merchant_id: @m3.id)
+    @m1_bogo = Coupon.create!(name: "Buy one item, get that same item free!", code: "M1_BOGO", amount: 50, percent: 1, active: 1, merchant_id: @m1.id)
+    @m1_half = Coupon.create!(name: "50% off any item", code: "M1_HALF_OFF1", amount: 50, percent: 1, active: 1,merchant_id: @m1.id)
+    @m1_buy3 = Coupon.create!(name: "Buy three items of any kind, get the fourth free!", code: "M1_BUY3_4FREE", amount: 25, percent: 1, active: 1,merchant_id: @m1.id)
+    @m1_fast50 = Coupon.create!(name: "Any orders of $50 or more get $10 off", code: "M1_FAST50", amount: 10, percent: 0, active: 1,merchant_id: @m1.id)
+    @m1_quarter = Coupon.create!(name: "Buy three items of any kind, get the fourth free!", code: "M1_QRT_OFF", amount: 25, percent: 1, active: 1,merchant_id: @m1.id)
+    @m2_10 = Coupon.create!(name: "$10 off any item!", code: "M2_10OFF", amount: 10, percent: 0, active: 1,merchant_id: @m2.id)
+    @m3_2024 = Coupon.create!(name: "$24 off an order of $100 or more", code: "M3_24", amount: 24, percent: 0, active: 1, merchant_id: @m3.id)
+    @m3_blowout = Coupon.create!(name: "50% off entire order!", code: "M3_TOTAL50", amount: 50, percent: 1, active: 1, merchant_id: @m3.id)
   end
 
   it "has a link to view all of a merchant's coupons" do 
@@ -38,10 +40,34 @@ RSpec.describe "merchant coupons index", type: :feature do
       fill_in(:name, with: "FOMO $40 off deal")
       fill_in(:code, with: "M1_FOMO40")
       fill_in(:amount, with: 40)
+      check(:active)
       check(:percent)
 
     # when I click the Submit button the user is taken back to the coupon index page and can see the new coupon listed 
-      click_button "Submit" 
+    # this coupon addition will not happen and trigger a flash message that there are already five active coupons
+    # and that one needs to be made inactive to add it to the system.
+      click_button "Submit"
+  
+      expect(current_path).to eq(merchant_coupons_path(@m1.id))
+      expect(page).to have_content("Merchants may have only five active coupons. Please de-activate a coupon from this merchant before trying to create another one.")
+      expect(page).to_not have_link("FOMO $40 off deal")
+      expect(page).to_not have_content("Discount Amount: $40")
+
+      # de-activate the merchant's first coupon in the list (giving them 4 total 'active' coupons)
+
+      @m1.coupons.first.update!(active: 0)
+
+      click_link("Create a new coupon")
+
+      fill_in(:name, with: "FOMO $40 off deal")
+      fill_in(:code, with: "M1_FOMO40")
+      fill_in(:amount, with: 40)
+      check(:active)
+      check(:percent)
+
+      click_button "Submit"
+
+      # new coupon saves since, now, it no longer breaks the `5 active coupons/merchant` threshold
       expect(current_path).to eq(merchant_coupons_path(@m1.id))
       visit merchant_coupons_path(@m1.id)
       expect(page).to have_link("FOMO $40 off deal")
