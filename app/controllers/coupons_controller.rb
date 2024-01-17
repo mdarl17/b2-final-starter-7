9dce6c6c02e59713811c1638f45ad7c3f357cd3a
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 10:37:15 -0700

    Add controller actions #new, #show, and #create (along with sad path in create) to add creating a new coupon functionality for merchants.  Also added strong_params for safety and coding speed (passing param def vs. each record's params individually)

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,4 +3,29 @@ class CouponsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @coupons = @merchant.coupons
   end
+
+  def show 
+    
+  end
+
+  def new 
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = Coupon.new(coupon_params)
+    
+    if coupon.save
+      merchant.coupons << coupon 
+    else
+      flash[:alert] = "There was a problem and the merchant coupon could not be saved."
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :code, :amount, :percent?, :merchant_id)
+  end
 end
