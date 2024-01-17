b790a7e6bbc9564b005632c204d65ab2c78b05e5
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 10:44:31 -0700

    Add merchant_id foreign key column to coupons table.

--- db/migrate/20240117073534_add_merchant_id_to_coupons.rb
+++ db/migrate/20240117073534_add_merchant_id_to_coupons.rb
@@ -1,4 +1,4 @@
-class AddMerchantIdToCoupons < ActiveRecord::Migration[7.1]
+class AddMerchantIdToCoupons < ActiveRecord::Migration[7.0]
   def change
     add_reference :coupons, :merchant, index: true, foreign_key: true
   end
