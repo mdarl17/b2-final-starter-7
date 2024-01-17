d8da773f566b1d11cfb73337e00f84d18cbc8850
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 10:42:53 -0700

    Change :percentage_discount column name to :amount since it could be a dollar or percent figure. New colum :percent (repped as enum in coupon model) to determine whether coupon value amount should be a dollar or percentage figure.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_17_073534) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_073534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coupons", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.integer "percentage_discount"
+    t.integer "amount"
+    t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
