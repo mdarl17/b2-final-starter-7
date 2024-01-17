64e0508d67cbd0417a28919351887dfa32af8804
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 16:41:40 -0700

    SIMPLIFY instance method #usage_count instance method

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,17 +1,14 @@
 class Coupon < ApplicationRecord 
   belongs_to :merchant
   has_many :invoices
-  # has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
-  enum percent: { "unchecked": 0, "checked": 1 }
+  enum type: { "unchecked": 0, "checked": 1 }
   enum active: { "inactive": 0, "active": 1 }
 
   def usage_count
-    self.invoices.select("coupons.id, COUNT(invoices.id) AS coupon_count")
-      .joins(:coupon, :transactions, :invoice_items)
-      .where("transactions.result > 0 AND coupons.id = ?", self.id)
-      .group("coupons.id, invoices.id")
-      .having("SUM(invoice_items.unit_price * invoice_items.quantity) >= 10")
-      .length
+    self.transactions
+      .where("transactions.result > 0")
+      .count
   end
 end
