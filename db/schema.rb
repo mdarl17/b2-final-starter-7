16910ebe630aa5fec551361d323f3421b11df328
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-17 17:01:36 -0700

    Change column name from type to discount_type bc type is a reserved keyword

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_17_234324) do
     t.string "name"
     t.string "code"
     t.integer "amount"
-    t.integer "type"
+    t.integer "discount_type"
     t.integer "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
